3faabe107540ebeaafe1ad8d5b2d2147aa811b0a
Antoine MILLET <rockyluke@offline.net>
2024-12-23 10:48:48 +0100

    feat: bw 2024.12.0

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -2,9 +2,9 @@ class BitwardenCli < Formula
   name "Bitwarden CLI"
   desc "CLI for Bitwarden"
   homepage "https://github.com/bitwarden/clients"
-  version "2024.10.0"
+  version "2024.12.0"
   url "https://github.com/bitwarden/clients/releases/download/cli-v#{version}/bw-macos-#{version}.zip"
-  sha256 "6c6885895cda9ba0a1326e08edd353cc51e59102a52d5b826cdcf362d3cd6b98"
+  sha256 "3a2a82f3616cda6f760d1b8f1f3ae4b5451a28ecbe56df5e8941ff98267d9241"
 
   def install
     bin.install "bw" => "bw"
