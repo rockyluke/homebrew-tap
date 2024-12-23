20c1b19228d5fe497c031fb3265e81695535f35d
rockyluke <rockyluke@offline.net>
2024-12-23 11:15:15 +0100

    fix: formula

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -1,9 +1,8 @@
 class BitwardenCli < Formula
-  name "Bitwarden CLI"
   desc "CLI for Bitwarden"
   homepage "https://github.com/bitwarden/clients"
   version "2024.12.0"
-  url "https://github.com/bitwarden/clients/releases/download/cli-v2024.12.0/bw-macos-2024.12.0.zip"
+  url "https://github.com/bitwarden/clients/releases/download/cli-v#{version}/bw-macos-#{version}.zip"
   sha256 "3a2a82f3616cda6f760d1b8f1f3ae4b5451a28ecbe56df5e8941ff98267d9241"
 
   conflicts_with "bitwarden-cli"
