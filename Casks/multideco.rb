79f76f416c92e5d9f0f0e29c2c20018dcbf914e6
Antoine MILLET <rockyluke@offline.net>
2026-02-08 15:07:20 +0100

    feat: update multideco

--- Casks/multideco.rb
+++ Casks/multideco.rb
@@ -2,8 +2,9 @@ cask "multideco" do
   name "MultiDeco"
   desc "Dive decompression planning software"
   homepage "https://www.hhssoftware.com/multideco/"
-  version "4.22.0"
-  url "https://www.hhssoftware.com/multideco/installfile.php?file=multideco_#{version}_arm_intel.dmg&keytag=1608915&qwerty=#{Time.now.to_i}&os=mac_intel"
+  version "4.26.2"
+  url "https://www.hhssoftware.com/multideco/installfile.php?file=multideco_#{version}_arm_intel.dmg&keytag=1628020&qwerty=#{Time.now.to_i}&os=mac_intel"
+  # url "https://www.hhssoftware.com/multideco/installfile.php?file=multideco_#{version}_arm_intel.dmg&keytag=1608915&qwerty=#{Time.now.to_i}&os=mac_intel"
   sha256 "51a5a7551ffd054cf034e37fa0e53bd322703625b553f5abcde0967f6ca91562"
 
   livecheck do
